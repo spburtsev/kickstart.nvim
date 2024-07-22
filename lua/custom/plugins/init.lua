4de1e1177efa2825a9a16af2e113b5d51757d1bf
Sergei Burtsev <spburtsev@proton.me>
2024-07-22 03:33:03 +0300

    Tabs configuted, wrap removed

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,6 +3,15 @@
 --
 -- See the kickstart.nvim README for more information
 vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.colorcolumn = '80'
+
 vim.keymap.set('n', '<leader>e', '<cmd>Ex<CR>')
 vim.keymap.set('n', '<C-s>', '<cmd>w<CR>')
+
 return {}
